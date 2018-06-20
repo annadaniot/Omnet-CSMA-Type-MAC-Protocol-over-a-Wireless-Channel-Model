fde303a92ce6fe59a498c0d07dff4ac677eef117
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-21 06:13:37 +1200

    Create Channel.cc
    The channel module distributes packets / signals between all attached stations.

---
+++ Channel.cc
@@ -0,0 +1,32 @@
+/****Author: Anna ****/
+
+
+
+#include "Channel.h" 
+ 
+Define_Module(Channel); 
+ 
+void Channel::initialize() {     
+  numTransmitters=par("numTransmitters");     
+  EV << "Starting channel with N number of transmitters: " << numTransmitters + 1; 
+} 
+ 
+void Channel::handleMessage(cMessage *msg) 
+{ 
+  EV<<"Channel::handleMessage"<<endl;     //if messages is of type SignalStart or SignalEnd from Transceiver     
+  if(msg->getKind()==SIGNAL_START || msg->getKind()==SIGNAL_STOP)     
+  {         
+    int i = 0;         
+  
+    for(i=0; i < numTransmitters; i++) //+1 for receiver 
+    {             
+     cMessage* copy=msg->dup();             
+     send(copy,"out",i);             
+     EV << "Sent duplicated incoming message to transmitter/receiver " << i;             
+     //   send(i==0?msg : msg->dup(), "out" ,i);         
+    }         
+   
+  send(msg,"out",i++);         
+  EV << "Sent duplicated incoming message to transmitter/receiver " << i;     
+  } 
+} 
