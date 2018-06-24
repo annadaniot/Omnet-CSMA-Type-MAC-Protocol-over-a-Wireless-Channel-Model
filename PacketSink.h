ae5df80277953978b8ddbaba99eca9fb5aa32592
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-24 23:07:44 +1200

    Create PacketSink.h

---
+++ PacketSink.h
@@ -0,0 +1,11 @@
+#ifndef __WSN_PACKETSINK_H_ 
+#define __WSN_PACKETSINK_H_ 
+ 
+#include <omnetpp.h> #include <fstream>  //can be ifstream or ofstream #include "messages.h" #include "AppMessage_m.h" 
+ 
+ 
+using namespace omnetpp; 
+ 
+/**  * TODO - Generated class  */ class PacketSink : public cSimpleModule {   protected:     std::stringstream out; //stream class to operate on 'out' gate 
+ 
+    virtual void initialize();     virtual void handleMessage(cMessage *msg); 
