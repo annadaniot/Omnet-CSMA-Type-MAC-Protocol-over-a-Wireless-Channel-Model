923dcedc0ec9eb6c472cb10fe90822926c79f0f2
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-24 22:52:56 +1200

    Update PacketGenerator.cc

--- PacketGenerator.cc
+++ PacketGenerator.cc
@@ -4,6 +4,26 @@
  
 Define_Module(PacketGenerator); 
  
-void PacketGenerator::initialize() {     iatDistribution = par("iatDistribution")   ;     messageSize = par("messageSize");     seqno = par("seqno") ;     nodeID = getParentModule()->par("nodeIdentifier"); //transmitter ID     generatePacket = new cMessage("GenAppMessage");     scheduleAt(simTime() + exponential(iatDistribution/1000), generatePacket); } 
+void PacketGenerator::initialize() 
+{     
+   iatDistribution = par("iatDistribution")   ;    
+   messageSize = par("messageSize"); 
+   seqno = par("seqno") ;   
+   nodeID = getParentModule()->par("nodeIdentifier"); //transmitter ID     
+   generatePacket = new cMessage("GenAppMessage");    
+   scheduleAt(simTime() + exponential(iatDistribution/1000), generatePacket); 
+} 
  
-AppMessage *PacketGenerator::generateMessage() {     out<< nodeID<< "," <<seqno<<endl;     AppMessage *applMsg = new AppMessage();     applMsg -> setTimeStamp(simTime());     applMsg ->setSenderId(nodeID);     applMsg -> setSequenceNumber(seqno++);     applMsg -> setMsgSize(messageSize);     applMsg ->setKind(APP_MSG);     return applMsg; } void PacketGenerator::handleMessage(cMessage *msg) {     if(msg==generatePacket)     {         scheduleAt(simTime() + exponential(iatDistribution/1000), msg);         AppMessage *applMsg =  generateMessage();         send(applMsg,"out");         EV<<"Generating application message."<<endl;         packetSent++; 
+AppMessage *PacketGenerator::generateMessage() 
+{     
+   out<< nodeID<< "," <<seqno<<endl;  
+   AppMessage *applMsg = new AppMessage();   
+   applMsg -> setTimeStamp(simTime());  
+   applMsg ->setSenderId(nodeID);  
+   applMsg -> setSequenceNumber(seqno++);   
+   applMsg -> setMsgSize(messageSize);     
+   applMsg ->setKind(APP_MSG);    
+   return applMsg; 
+} 
+
+void PacketGenerator::handleMessage(cMessage *msg) {     if(msg==generatePacket)     {         scheduleAt(simTime() + exponential(iatDistribution/1000), msg);         AppMessage *applMsg =  generateMessage();         send(applMsg,"out");         EV<<"Generating application message."<<endl;         packetSent++; 
