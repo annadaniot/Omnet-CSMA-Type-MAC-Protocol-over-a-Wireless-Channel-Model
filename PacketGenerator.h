7fe9280b30a3018f262beec8576489bbe245ca76
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-24 22:56:49 +1200

    Create PacketGenerator.h

---
+++ PacketGenerator.h
@@ -0,0 +1,18 @@
+#ifndef __WSN_PACKETGENERATOR_H_ #define __WSN_PACKETGENERATOR_H_ 
+ 
+#include <omnetpp.h> #include "AppMessage_m.h" #include "messages.h" 
+ 
+ 
+using namespace omnetpp; 
+ 
+/**  * TODO - Generated class  */ class PacketGenerator : public cSimpleModule {   protected:     double iatDistribution;     int messageSize;     int seqno;     int nodeID; //transmitter ID     int packetSent=0; 
+ 
+    cMessage *generatePacket; 
+ 
+    std::stringstream out; //stream class to operate in out gate 
+ 
+    AppMessage *generateMessage(); 
+ 
+    virtual void initialize();     virtual void handleMessage(cMessage *msg);     virtual void finish(); }; 
+ 
+#endif 
