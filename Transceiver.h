dc76499cda152581a7cfe627285a8620ab641c39
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-25 22:42:57 +1200

    Update Transceiver.h

--- Transceiver.h
+++ Transceiver.h
@@ -16,3 +16,15 @@ class Transceiver : public cSimpleModule {     //is okay to omit the constructor
     //either transmit or receive.     //If receive state, the transceiver is ready to receive data or is receiving data     //If transmit state, the transmitter is preparing to transmit data or is transmitting data     //default is to be in the receive state. TODO: on init set to recv state     //Transmit state is entered upon request from the MAC module.     enum RecvTransState{receive, transmit}; 
  
 public:     virtual void initialize();     virtual void finish();     void handleMessage(cMessage *msg);     void handleTransmissionRequest(TransmissionRequest *msg);     void handleSignalStart(SignalStart *msg);     void handleSignalStop(SignalStop *msg); 
+ void handleCSRequest(CSRequest *msg);     void handleCSResponse(CSResponse* msg);     void handleBitRateWait(SignalStop* msg);     double calcReceivedPowerDBM(SignalStart* SNRsignal); 
+ 
+ 
+private:     volatile double turnAroundTime;     volatile double txPowerDBm; //radiated power in dBm^2     int bitRate;//specifies the transmission bitrate in bits/s     volatile double csThreshDBm;//specifies the observed signal power (in dBm) aboce which a carrier-sensing station will indicate the medium as busy.     volatile double noisePowerDBm;//specifies the nose power (which here we purport to be expressed in units of dBm, which is not entierly correct).     volatile double csTime;     int Nnodes; //     int statNumSentPackets=0;      int packetsLost=0;     string TransceiverLogFileName; 
+ 
+ 
+    //list of current transmissions. On compile set length to number of nodes in the simulation     //        currentTransmissions currentTransmissionsList[getParentModule()>par("TransmitterNodes")];     SignalStart* currentTransmissionsList[MAX_TRANSCEIVER_COUNT]; 
+ 
+    RecvTransState transceiverState; }; 
+ 
+#endif 
+ 
