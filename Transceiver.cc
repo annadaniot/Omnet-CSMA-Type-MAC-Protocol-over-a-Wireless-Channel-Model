6bd8fae5815ed2e510b59a7e2f04614efaac8019
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-25 22:01:46 +1200

    Update Transceiver.cc

--- Transceiver.cc
+++ Transceiver.cc
@@ -44,7 +44,14 @@ void Transceiver::initialize()
     //    packetsLost = 0; 
  } 
  
-//called when the simulation finishes void Transceiver::finish() {     //do not delete anything or cancel timers, all cleanup must be done in the destructor     //use this for statistics     EV <<"NUM SENT PACKETS " << statNumSentPackets << endl;     std::ofstream file; 
+//called when the simulation finishes 
+
+void Transceiver::finish() 
+{     
+   //do not delete anything or cancel timers, all cleanup must be done in the destructor  
+   //use this for statistics    
+   EV <<"NUM SENT PACKETS " << statNumSentPackets << endl;
+   std::ofstream file; 
 
  if(strcmp(getParentModule()->getName(),"receiverNode")  == 0 && Nnodes>1){//log packetsLost         EV << "Finishing transceiver, Logging to" << TransceiverLogFileName;         file.open(TransceiverLogFileName, std::ios::app); //this appends to end of file         file << Nnodes << ", "<< statNumSentPackets<<", "<< packetsLost << endl;         EV << "Logged "<< Nnodes << ", "<<statNumSentPackets<<", "<< packetsLost << endl;         file.close();     }     else if(Nnodes == 1){ //log statNumSentPackets         EV << "Finishing transceiver, Logging statNumSentPackets to " << TransceiverLogFileName <<endl;         file.open(TransceiverLogFileName, std::ios::app); //this appends to end of file         double R = getParentModule()->getParentModule()->par("R");         file << R << ", "<< statNumSentPackets << endl;         EV <<"logged " << R << ", "<< statNumSentPackets << endl;         file.close();     } } 
   
