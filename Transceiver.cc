06fa5d4f6faa27a72ce8f411f869d23f2166882d
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-25 21:19:48 +1200

    Create Transceiver.cc

---
+++ Transceiver.cc
@@ -0,0 +1,14 @@
+#include "Transceiver.h" 
+ 
+#include <cmath> #include <ctgmath> 
+ 
+Define_Module(Transceiver); int statNumSentPackets = 0; //called when the simulation starts void Transceiver::initialize() {     EV << getParentModule()->getName() << ": initialsing Transceiver\n";     turnAroundTime = par("turnAroundTime");     txPowerDBm = par("txPowerDBm");     bitRate = par("bitRate");     csThreshDBm = par("csThreshDBm");     noisePowerDBm = par("noisePowerDBm");     csTime = par("csTime"); 
+ 
+    Nnodes = getParentModule()->par("numTransmitters");     for(int i = 0; i<Nnodes; i++){         currentTransmissionsList[i] = nullptr;     } 
+ 
+    transceiverState = receive; 
+ 
+    //logging     if(strcmp(getParentModule()->getName(),"receiverNode")  == 0 && //check if conditions meet second tests, log packetsLost             Nnodes>1){         TransceiverLogFileName = generate_log_file_name(par("TransceiverFile2").stringValue(), true);         EV << "Transceiver Using log file: " << TransceiverLogFileName << endl;     }     else if(Nnodes == 1){//settings meet first test, log statNumSentPackets         TransceiverLogFileName = generate_log_file_name(par("TransceiverFile1").stringValue(), true);         EV << "Transceiver Using log file: " << TransceiverLogFileName << endl;     } //    statNumSentPackets = 0; //    packetsLost = 0; 
+ 
+} 
+ 
