add3d45dcdf656b92b3d9c93f62de284df502f7c
annadaniot <40380893+annadaniot@users.noreply.github.com>
2018-06-26 21:00:52 +1200

    Update Transceiver.h

--- Transceiver.h
+++ Transceiver.h
@@ -19,9 +19,15 @@ class Transceiver : public cSimpleModule
    //is okay to omit the constructor altogether because the compiler generated one is suitable. 
    //will have a list of these structs to store current transmissions    
    //records important features of current transmissions, includes own transmissions     
-   typedef struct currentTransmissions{         int senderID;         float receivedPower; //dbm     }currentTransmissions; 
- 
-    //either transmit or receive.     //If receive state, the transceiver is ready to receive data or is receiving data     //If transmit state, the transmitter is preparing to transmit data or is transmitting data     //default is to be in the receive state. TODO: on init set to recv state     //Transmit state is entered upon request from the MAC module.     enum RecvTransState{receive, transmit}; 
+   typedef struct currentTransmissions
+   {         
+      int senderID;  
+      float receivedPower; 
+      //dbm   
+    }
+    
+    currentTransmissions; 
+     //either transmit or receive.     //If receive state, the transceiver is ready to receive data or is receiving data     //If transmit state, the transmitter is preparing to transmit data or is transmitting data     //default is to be in the receive state. TODO: on init set to recv state     //Transmit state is entered upon request from the MAC module.     enum RecvTransState{receive, transmit}; 
  
 public:     virtual void initialize();     virtual void finish();     void handleMessage(cMessage *msg);     void handleTransmissionRequest(TransmissionRequest *msg);     void handleSignalStart(SignalStart *msg);     void handleSignalStop(SignalStop *msg); 
  void handleCSRequest(CSRequest *msg);     void handleCSResponse(CSResponse* msg);     void handleBitRateWait(SignalStop* msg);     double calcReceivedPowerDBM(SignalStart* SNRsignal); 
